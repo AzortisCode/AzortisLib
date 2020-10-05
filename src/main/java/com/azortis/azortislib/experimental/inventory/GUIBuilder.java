7886d624be1ba3200d2bea7038326470917a6b5f
X1XX <dragonhearth1946@gmail.com>
2020-10-04 22:54:06 -0500

    yeet

--- src/main/java/com/azortis/azortislib/experimental/inventory/GUIBuilder.java
+++ src/main/java/com/azortis/azortislib/experimental/inventory/GUIBuilder.java
@@ -1,3 +1,21 @@
+/*
+ * An open source utilities library used for Azortis plugins.
+ *     Copyright (C) 2019  Azortis
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.azortis.azortislib.experimental.inventory;
 
 import com.azortis.azortislib.experimental.inventory.item.Item;
@@ -47,6 +65,7 @@ public class GUIBuilder {
         public GUIBuilder with(Consumer<PageBuilder> pageBuilderConsumer) {
             pageBuilderConsumer.accept(this);
             Page p = new Page(guiBuilder.gui, page, name, isGlobal, pageSize, closeAction);
+            p.setItems(items);
             guiBuilder.gui.getPages().add(p);
             return guiBuilder;
         }
